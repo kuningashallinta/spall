f59425e37f08cf3ae4d22dd56e838294fde21a5a
King Hallinta <172042569+kuningashallinta@users.noreply.github.com>
2026-09-04 21:17:32 +0200

    Add the missing operators to SPALL_ENUM_CLASS_BITWISE_OPERATORS

--- include/spall/Common/Bit.h
+++ include/spall/Common/Bit.h
@@ -18,7 +18,25 @@
 		using UnderlyingType = std::underlying_type_t<type>;                                              \
 		return static_cast<type>(static_cast<UnderlyingType>(left) & static_cast<UnderlyingType>(right)); \
 	}                                                                                                     \
+	constexpr inline type operator^(type left, type right)                                                \
+	{                                                                                                     \
+		using UnderlyingType = std::underlying_type_t<type>;                                              \
+		return static_cast<type>(static_cast<UnderlyingType>(left) ^ static_cast<UnderlyingType>(right)); \
+	}                                                                                                     \
+	constexpr inline type operator~(type value)                                                           \
+	{                                                                                                     \
+		using UnderlyingType = std::underlying_type_t<type>;                                              \
+		return static_cast<type>(~static_cast<UnderlyingType>(value));                                    \
+	}                                                                                                     \
 	inline type& operator|=(type& left, type right)                                                       \
 	{                                                                                                     \
 		return left = left | right;                                                                       \
+	}                                                                                                     \
+	inline type& operator&=(type& left, type right)                                                       \
+	{                                                                                                     \
+		return left = left & right;                                                                       \
+	}                                                                                                     \
+	inline type& operator^=(type& left, type right)                                                       \
+	{                                                                                                     \
+		return left = left ^ right;                                                                       \
 	}
